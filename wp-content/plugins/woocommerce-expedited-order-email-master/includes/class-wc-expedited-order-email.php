f960e7d5da9db7ea12eeabcf04062bfaf57d7a97
DESKTOP-ATRT1VG\Dago <riderfish@gmail.com>
2016-11-27 11:45:55 +0800

    expedited

---
+++ wp-content/plugins/woocommerce-expedited-order-email-master/includes/class-wc-expedited-order-email.php
@@ -0,0 +1,169 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * A custom Expedited Order WooCommerce Email class
+ *
+ * @since 0.1
+ * @extends \WC_Email
+ */
+class WC_Expedited_Order_Email extends WC_Email {
+
+
+	/**
+	 * Set email defaults
+	 *
+	 * @since 0.1
+	 */
+	public function __construct() {
+
+		// set ID, this simply needs to be a unique name
+		$this->id = 'wc_expedited_order';
+
+		// this is the title in WooCommerce Email settings
+		$this->title = '新訂單(顧客)';
+
+		// this is the description in WooCommerce email settings
+		$this->description = '顧客下單後收到的確認信';
+
+		// these are the default heading and subject lines that can be overridden using the settings
+		$this->heading = __( 'Thank you for your order', 'woocommerce' );
+		$this->subject = __( 'Your {site_title} order receipt from {order_date}', 'woocommerce' );
+
+		// these define the locations of the templates that this email should use, we'll just use the new order template since this email is similar
+		$this->template_html  = 'emails/customer-new-order.php';
+		$this->template_plain = 'emails/plain/customer-new-order.php';
+
+		// Trigger on new paid orders
+		add_action( 'woocommerce_new_order', array( $this, 'trigger' ) );
+		//add_action( 'woocommerce_order_status_failed_to_processing_notification',  array( $this, 'trigger' ) );
+
+		// Call parent constructor to load any other defaults not explicity defined here
+		parent::__construct();
+
+		// this sets the recipient to the settings defined below in init_form_fields()
+//		$this->recipient = $this->get_option( 'recipient' );
+//
+//		// if none was entered, just use the WP admin email as a fallback
+//		if ( ! $this->recipient )
+//			$this->recipient = get_option( 'admin_email' );
+//
+//        $this->recipient = 'customer';
+        $this->customer_email   = true;
+	}
+
+
+	/**
+	 * Determine if the email should actually be sent and setup email merge variables
+	 *
+	 * @since 0.1
+	 * @param int $order_id
+	 */
+	public function trigger( $order_id ) {
+
+		// bail if no order ID is present
+		if ( ! $order_id )
+			return;
+
+		// setup order object
+		$this->object = new WC_Order( $order_id );
+
+		// bail if shipping method is not expedited
+//		if ( ! in_array( $this->object->get_shipping_method(), array( 'Three Day Shipping', 'Next Day Shipping' ) ) )
+//			return;
+
+		// replace variables in the subject/headings
+		$this->find[] = '{order_date}';
+		$this->replace[] = date_i18n( woocommerce_date_format(), strtotime( $this->object->order_date ) );
+
+		$this->find[] = '{order_number}';
+		$this->replace[] = $this->object->get_order_number();
+
+		if ( ! $this->is_enabled())
+			return;
+
+		// woohoo, send the email!
+		//$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+        $this->send( $this->object->billing_email, $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+	}
+
+
+	/**
+	 * get_content_html function.
+	 *
+	 * @since 0.1
+	 * @return string
+	 */
+	public function get_content_html() {
+		ob_start();
+		woocommerce_get_template( $this->template_html, array(
+			'order'         => $this->object,
+			'email_heading' => $this->get_heading()
+		) );
+		return ob_get_clean();
+	}
+
+
+	/**
+	 * get_content_plain function.
+	 *
+	 * @since 0.1
+	 * @return string
+	 */
+	public function get_content_plain() {
+		ob_start();
+		woocommerce_get_template( $this->template_plain, array(
+			'order'         => $this->object,
+			'email_heading' => $this->get_heading()
+		) );
+		return ob_get_clean();
+	}
+
+
+	/**
+	 * Initialize Settings Form Fields
+	 *
+	 * @since 2.0
+	 */
+	public function init_form_fields() {
+
+		$this->form_fields = array(
+			'enabled'    => array(
+				'title'   => '啓用/停用',
+				'type'    => 'checkbox',
+				'label'   => '啟用此電子郵件通知',
+				'default' => 'yes'
+			),
+
+			'subject'    => array(
+				'title'       => '電子郵件主旨',
+				'type'        => 'text',
+				'description' => sprintf( 'This controls the email subject line. Leave blank to use the default subject: <code>%s</code>.', $this->subject ),
+				'placeholder' => '',
+				'default'     => ''
+			),
+			'heading'    => array(
+				'title'       => '電子郵件內文主旨',
+				'type'        => 'text',
+				'description' => sprintf( __( 'This controls the main heading contained within the email notification. Leave blank to use the default heading: <code>%s</code>.' ), $this->heading ),
+				'placeholder' => '',
+				'default'     => ''
+			),
+			'email_type' => array(
+				'title'       => '電子郵件類型',
+				'type'        => 'select',
+				'description' => '選擇電子郵件的傳送格式',
+				'default'     => 'html',
+				'class'       => 'email_type',
+				'options'     => array(
+					'plain'	    => __( 'Plain text', 'woocommerce' ),
+					'html' 	    => __( 'HTML', 'woocommerce' ),
+					'multipart' => __( 'Multipart', 'woocommerce' ),
+				)
+			)
+		);
+	}
+
+
+} // end \WC_Expedited_Order_Email class
