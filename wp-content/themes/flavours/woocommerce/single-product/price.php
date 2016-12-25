db846bdb120f76499bc598c398ef29f91288d4b1
DESKTOP-ATRT1VG\Dago <riderfish@gmail.com>
2016-12-25 14:12:15 +0800

    stock update

--- wp-content/themes/flavours/woocommerce/single-product/price.php
+++ wp-content/themes/flavours/woocommerce/single-product/price.php
@@ -18,7 +18,24 @@ global $product;
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer" class="price-block">
     <div class="price-box price"> <?php echo $product->get_price_html(); ?></div>
         
-        <?php if($product->is_in_stock()){  ?> <p class="availability in-stock pull-right"><span><?php esc_attr_e('In Stock','flavours');?></span></p> <?php } else { ?><p class="availability out-of-stock pull-right"><span><?php esc_attr_e('Out of Stock','flavours');?></span></p><?php } ?>
+        <?php if($product->is_in_stock()){
+            $stock = number_format($product->stock, 0);
+
+            ?>
+            <p class="availability in-stock pull-right">
+                <span>
+                    <?php esc_attr_e('熱銷中','flavours');?>
+                    <?php if ($stock > 0) {
+                        echo '，目前限量 ' . $stock . ' 份';
+                    }
+                    ?>
+                </span>
+            </p>
+        <?php } else { ?>
+            <p class="availability out-of-stock pull-right">
+                <span><?php esc_attr_e('補貨中','flavours');?></span>
+            </p>
+        <?php } ?>
         <meta itemprop="price" content="<?php echo $product->get_price(); ?>"/>
         <meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>"/>
         <link itemprop="availability"
